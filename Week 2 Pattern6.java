0a99c08cc42350986c3f9b8ac85ec10262600e55
SakthiprabaManjunath <121786476+SakthiprabaManjunath@users.noreply.github.com>
2026-06-04 13:20:01 +0530

    Add files via upload

---
+++ Week 2 Pattern6.java
@@ -0,0 +1,31 @@
+/*
+Sample 1:
+
+Input:
+
+n = 4
+
+Expected output:
+
+****
+
+***
+
+**
+
+*
+
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n-i+1;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+	}
+}
